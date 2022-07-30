7c81d4610e2adfc005759970d0e1253017edf1a6
Doppler <thomas.bertin@yahoo.fr>
2022-07-30 10:41:48 +0200

    the image folder is now ignored by gitand the 401 errors for unauthorized request are now 403

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -61,7 +61,7 @@ exports.modifySauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
+        res.status(403).json({ message: 'Unauthorized request' });
       } else {
         Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Objet modifié!' }))
@@ -77,7 +77,7 @@ exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
       if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
+        res.status(403).json({ message: 'Unauthorized request' });
       } else {
         const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => {
