49f275e501483e57926c7f37164b469678b6ccdf
Doppler <thomas.bertin@yahoo.fr>
2022-08-02 08:56:46 +0200

    add of a middleware to check the email format before sign up

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 const passwordValidation = require("../middleware/password-validation");
+const emailValidation = require("../middleware/email-validation");
 
-router.post('/signup', passwordValidation, userCtrl.signup);
+router.post('/signup', emailValidation, passwordValidation, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
